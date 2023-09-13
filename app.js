c44d9710730951e729451bcba8830e955dac7682
asiq <mohammedasiq152@gmail.com>
2023-09-13 17:25:05 +0530

    netlify deploy

--- app.js
+++ app.js
@@ -2,39 +2,17 @@ const express = require("express");
 const app = express();
 const port = 8080;
 const http = require("http");
-const cors = require("cors");
-const WebSocket = require("ws");
 
 const server = http.createServer(app);
+
+app.get("/", (req, res) => {
+  res.send("Api working");
+});
+app.get("/json", (req, res) => {
+  res.json({ version: "1.0", status: "Working" });
+});
 
 server.listen(port, (err) => {
   if (err) return console.log("Internal Server Error!");
   console.log("Server running on port - " + port);
 });
-
-const wss = new WebSocket.Server({ server });
-
-wss.on("connection", (ws) => {
-  console.log("A client connected.");
-
-  ws.on("message", (message) => {
-    console.log(`Received: ${message}`);
-    // Broadcast the received message to all connected clients
-    wss.clients.forEach((client) => {
-      console.log(client);
-      if (client === ws && client.readyState === WebSocket.OPEN) {
-        console.log('send');
-        client.send(JSON.stringify(message));
-      }
-    });
-  });
-
-  // ws.on('close', () => {
-  //   console.log('A client disconnected.');
-  // });
-});
-
-// app.use(cors());
-// app.use(express.json());
-
-// app.get("/", () => {});
